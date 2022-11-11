a6b4b50f0ad27971c11f1717975538673c9c3856
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2022-11-12 00:08:13 +0900

    TodoApp Empty View

--- TodoApp/components/Empty.js
+++ TodoApp/components/Empty.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 function Empty() {
   return (
     <View style={styles.block}>
+      <Image
+        source={require("./../assets/images/young_and_happy.png")}
+        style={styles.image}
+        resizeMode="cover"
+      />
       <Text style={styles.description}>야호! 할일이 없습니다.</Text>
     </View>
   );
@@ -15,10 +20,16 @@ const styles = StyleSheet.create({
     // alignItems, justifyContent: center -> 내용 정중앙에 위치
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "white",
   },
   description: {
     fontSize: 24,
     color: "#9e9e9e",
+  },
+  image: {
+    width: 240,
+    height: 179,
+    marginBottom: 16,
   },
 });
 
